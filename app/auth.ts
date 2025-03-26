c77def36330a150426a70c6a1d14921ccc6ce2f0
Prajyot Tayde <pmtbmt@gmail.com>
2025-03-26 23:04:18 +0530

    fix home page ui

--- app/auth.ts
+++ app/auth.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import client from "./lib/db"
+import NextAuth from "next-auth";
+import client from "./lib/db";
+import Google from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: MongoDBAdapter(client),
-    providers: [Google]
+  adapter: MongoDBAdapter(client),
+  providers: [Google],
 });
